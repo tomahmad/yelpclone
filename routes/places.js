06306a709f8e9d140d528cdbef68ffd78ae4955d
Ahmad Taufiq Gultom <taufiq.gultom@gmail.com>
2024-09-24 23:26:20 +0700

    memisahkan validasi input dengan middleware

--- routes/places.js
+++ routes/places.js
@@ -1,22 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const ErrorHandler = require("../utils/errorHandler");
-const { placeSchema } = require("../schemas/place");
 const wrapAsync = require("../utils/wrapAsync");
 const PlaceController = require("../controllers/places.js");
 const isValidObjectId = require("../middleware/isValidObjectId");
 const isAuth = require("../middleware/isAuth.js");
 const { isAuthorPlace } = require("../middleware/isAuthor.js");
-
-const validatePlace = (req, res, next) => {
-  const { error } = placeSchema.validate(req.body);
-  if (error) {
-    const msg = error.details.map((el) => el.message).join(",");
-    return next(new ErrorHandler(msg, 404));
-  } else {
-    next();
-  }
-};
+const { validatePlace } = require("../middleware/validator");
 
 router
   .route("/")
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,23 +1,12 @@
 const express = require("express");
 // mergeParams dibutukan agar parameter 'place_id' yang ada di app.js, bisa digunakan di file ini (link pada method post tidak tersedia place_id)
 const router = express.Router({ mergeParams: true });
-const ErrorHandler = require("../utils/errorHandler");
 const wrapAsync = require("../utils/wrapAsync");
-const { reviewSchema } = require("../schemas/review");
 const isValidObjectId = require("../middleware/isValidObjectId");
 const isAuth = require("../middleware/isAuth");
 const { isAuthorReview } = require("../middleware/isAuthor");
-const ReviewController = require('../controllers/reviews')
-
-const validateReview = (req, res, next) => {
-  const { error } = reviewSchema.validate(req.body);
-  if (error) {
-    const msg = error.details.map((el) => el.message).join(",");
-    return next(new ErrorHandler(msg, 400));
-  } else {
-    next();
-  }
-};
+const ReviewController = require("../controllers/reviews");
+const { validateReview } = require("../middleware/validator");
 
 router.post(
   "/",
